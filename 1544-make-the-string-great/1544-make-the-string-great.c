0bff16692041ec358ea36a789befeaeede2755d5
Khuat Tien Dung <khuatiendung250@gmail.com>
2026-03-27 09:39:59 +0700

    Time: 0 ms (100%), Space: 8.7 MB (23.88%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.c
@@ -0,0 +1,14 @@
+char* makeGood(char* s) {
+    int top = -1;
+
+    for (int i = 0; s[i]; i++) {
+        if (top >= 0 && ((s[top] ^ s[i]) == 32)) {
+            top--;
+        } else {
+            s[++top] = s[i];
+        }
+    }
+
+    s[top + 1] = '\0';
+    return s;
+}
