5cb3b7f66d41629f1ca367f7e257cd3d3a00d482
Khuat Tien Dung <khuatiendung250@gmail.com>
2026-03-27 09:39:17 +0700

    Time: 4 ms (12.18%), Space: 10.5 MB (16.05%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.c
@@ -0,0 +1,23 @@
+int evalRPN(char** tokens, int tokensSize) {
+    int stack[10000];
+    int top =-1;
+    for(int i =0; i< tokensSize;i++){
+        char *t = tokens[i];
+        if(strcmp(t, "+") == 0 ||
+           strcmp(t, "-") == 0 ||
+           strcmp(t, "*") == 0 ||
+           strcmp(t, "/") == 0 )
+        {
+            int b = stack[top--];
+            int a = stack[top--];
+            if(strcmp(t, "+")==0) stack[++top] = a +b;
+            else if(strcmp(t, "-") ==0 ) stack[++top] = a - b;
+            else if(strcmp(t, "/") ==0) stack[++top] = a /b;
+            else if(strcmp(t, "*")==0) stack[++top] = a *b;
+        }
+        else{
+            stack[++top] = atoi(t);
+        }
+    }
+    return stack[top];
+}
