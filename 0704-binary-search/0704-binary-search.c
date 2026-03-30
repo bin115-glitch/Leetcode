609072eb7a7f57f6639b271eb4a607b17f96ea7d
Khuat Tien Dung <khuatiendung250@gmail.com>
2026-03-30 10:04:04 +0700

    Time: 0 ms (100%), Space: 9.9 MB (45.63%) - LeetHub

--- 0704-binary-search/0704-binary-search.c
+++ 0704-binary-search/0704-binary-search.c
@@ -1,8 +1,15 @@
 int search(int* nums, int numsSize, int target) {
-    for(int i = 0; i< numsSize;i++){
-        if(nums[i] == target){
-            return i;
-        }
-    }  
+    int left = 0;
+    int right = numsSize-1;
+    while(left<= right){
+        int mid = left + (right-left)/2;
+        if(nums[mid] == target) return mid;
+        else if(nums[mid]<target){
+            left = mid+1;
+            }
+        else {
+            right = mid-1;
+            }
+    }
     return -1;
 }
