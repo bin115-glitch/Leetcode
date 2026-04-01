fcb5355d7934cdf418ade9b4e67f80573a427942
Khuat Tien Dung <khuatiendung250@gmail.com>
2026-04-01 16:35:34 +0700

    Time: 2 ms (44.44%), Space: 11.6 MB (11.11%) - LeetHub

---
+++ 1237-find-positive-integer-solution-for-a-given-equation/1237-find-positive-integer-solution-for-a-given-equation.c
@@ -0,0 +1,44 @@
+/*
+ * // This is the definition for customFunction API.
+ * // You should not implement it, or speculate about its implementation
+ *
+ * // Returns f(x, y) for any given positive integers x and y.
+ * // Note that f(x, y) is increasing with respect to both x and y.
+ * // i.e. f(x, y) < f(x + 1, y), f(x, y) < f(x, y + 1)
+ */
+
+/**
+ * Return an array of arrays of size *returnSize.
+ * The sizes of the arrays are returned as *returnColumnSizes array.
+ * Note: Both returned array and *columnSizes array must be malloced, assume caller calls free().
+ */
+int** findSolution(int (*customFunction)(int, int), int z, int* returnSize, int** returnColumnSizes) {
+	int** result = (int**) malloc(1000*sizeof(int*));
+    *returnColumnSizes = (int*) malloc(1000 *sizeof(int));
+
+    int count=0;
+    int x=1;
+    int y=1000;
+    while(x<=1000 && y>=1){
+        int val = customFunction(x,y);
+        if(val == z){
+            result[count] = (int*) malloc(1000*sizeof(int));
+            result[count][0] = x;
+            result[count][1] = y;
+            (*returnColumnSizes)[count] = 2;
+            count++;
+
+        x++;
+        y--;
+        }
+        else if(val < z){
+            x++;
+        }
+        else {
+            y--;
+        }
+          
+    }
+    *returnSize = count;
+    return result;
+}
